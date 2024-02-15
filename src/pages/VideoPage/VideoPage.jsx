616c322eea86c965079a0aa6c666ac24c85ae439
Yurii Popsui <yp.onward@gmail.com>
2024-02-15 22:41:26 +0200

    Add VideoPage with videoplayer

--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -7,24 +7,34 @@ import { getMovieByIdSelector } from "../../store/selectors/moviesSelector";
 import { getMovieById } from "../../store/reducers/getMovieByIdReducer";
 
 const VideoPage = () => {
-
   const dispatch = useDispatch();
   const params = useParams();
   const movie = useSelector(getMovieByIdSelector);
+
+  console.log(movie);
 
   useEffect(() => {
     dispatch(getMovieById(params.id));
   }, [dispatch, params.id]);
 
   return (
-    <div>
+    <div className={styles.video}>
       {movie && (
-        <div>
-          <img
-            className={styles.card__img}
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            alt="backdrop"
-          />
+        <div className={styles.video__container}>
+          <h2 className={styles.video__title}>{movie.title}</h2>
+          <video
+            className={styles.video__player}
+            src={`https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`}
+            poster={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            width="90%"
+            controls
+          >
+            {movie.id}
+          </video>
+          <div className={styles.video__description}>
+            <h3 className={styles.video__description_title}>About movie: </h3>
+            <h3 className={styles.video__description_item}>{movie.overview}</h3>
+          </div>
         </div>
       )}
     </div>
